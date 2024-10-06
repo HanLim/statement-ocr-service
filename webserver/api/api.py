55f9f28c03d59327235e18dbb4a15acef29df750
HanLim <choohanlim96@gmail.com>
2024-10-06 19:01:49 +0800

    Refactor data logic into serializer

--- webserver/api/api.py
+++ webserver/api/api.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from ..database import get_db
-from ..statement.models import Statement, StatementTransaction, StatementDetails
+from ..statement.models import Statement
 from ..statement.serializers import *
+from ..utils import PdfToImageConverter, StatementExtractor
 
 router = APIRouter()
 
@@ -25,31 +26,7 @@ async def get_one_statement(statement_id: int, db: Session=Depends(get_db)):
 
 @router.post("/")
 async def create_statement(statement_data: StatementCreate, db: Session=Depends(get_db)):
-    statement = Statement(
-        address=statement_data.address,
-        name=statement_data.name,
-        statement_date=statement_data.statement_date,
-    )
-
-    detail = StatementDetails(
-        total_debit=statement_data.detail.total_debit,
-        total_credit=statement_data.detail.total_credit,
-        no_debit=statement_data.detail.no_debit,
-        no_credit=statement_data.detail.no_credit
-    )
-    
-    statement.detail = detail
-
-    for transaction in statement_data.transactions:
-        new_transaction = StatementTransaction(
-            transaction_date=transaction.transaction_date,
-            amount=transaction.amount
-        )
-        statement.transactions.append(new_transaction)
-
-    db.add(statement)
-    db.commit()
-    db.refresh(statement)
+    statement = StatementCreate.create(statement_data, db)
     return statement
 
 
--- webserver/statement/serializers.py
+++ webserver/statement/serializers.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import List, Optional
-from .models import Statement
+from typing import List
+from sqlalchemy.orm import Session
 
+from .models import Statement, StatementDetails, StatementTransaction
 
 class StatementDetailCreate(BaseModel):
     total_debit: float
@@ -16,13 +17,42 @@ class StatementTransactionCreate(BaseModel):
     amount: float
 
 
-
 class StatementCreate(BaseModel):
     address: str
     name: str
     statement_date: datetime
     detail: StatementDetailCreate
     transactions: List[StatementTransactionCreate]
+
+    @staticmethod
+    def create(statement_data: "StatementCreate", db: Session) -> "Statement":
+        statement = Statement(
+            address=statement_data.address,
+            name=statement_data.name,
+            statement_date=statement_data.statement_date,
+        )
+
+        detail = StatementDetails(
+            total_debit=statement_data.detail.total_debit,
+            total_credit=statement_data.detail.total_credit,
+            no_debit=statement_data.detail.no_debit,
+            no_credit=statement_data.detail.no_credit
+        )
+        
+        statement.detail = detail
+
+        for transaction in statement_data.transactions:
+            new_transaction = StatementTransaction(
+                transaction_date=transaction.transaction_date,
+                amount=transaction.amount
+            )
+            statement.transactions.append(new_transaction)
+
+        db.add(statement)
+        db.commit()
+        db.refresh(statement)
+
+        return statement
 
 
 class StatementListResponse(BaseModel):
@@ -55,7 +85,7 @@ class StatementResponse(BaseModel):
     transactions: List[StatementTransactionResponse]
 
     @staticmethod
-    def serialize(statement: Statement) -> "StatementResponse":
+    def serialize(statement: "Statement") -> "StatementResponse":
         detail = StatementDetailResponse(
             id=statement.detail.id,
             total_debit=statement.detail.total_debit,
